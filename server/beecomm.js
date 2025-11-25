3307c1637f768b08343b954fb42bfd3559f802f1
Moti <motiank@gmail.com>
2025-11-26 00:17:29 +0200

    morelog

--- server/beecomm.js
+++ server/beecomm.js
@@ -306,6 +306,7 @@ router.post('/pelecard/placeorder', express.text({ type: ['text/plain', 'text/*'
     // Case 2: Body is an object with resultDataKeyName (form-encoded with JSON in field)
     
     else if (req.body && typeof req.body === 'object' && ( req.body.eatalia_res || req.body.resultDataKeyName)) {
+      console.log("enter here ")
       const jsonField = req.body[ req.body.eatalia_res || req.body.resultDataKeyName ];
       if (typeof jsonField === 'string') {
         try {
